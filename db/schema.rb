bf5bbd3ea1c2437ba7341228a1f184984639858d
Pierre <pierre.beret@gmail.com>
2022-05-16 18:11:10 +0200

    articles_category bazar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_143038) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_154454) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
